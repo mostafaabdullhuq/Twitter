6d3988190417808e3f254b5e0e70c63a3b4de576
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-10 01:23:51 +0200

    edit tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./docs/*.{html,js}"],
+    darkMode: "class",
     theme: {
         extend: {
             colors: {
+                twitter: {
+                    blue: "#1d9bf0",
+                    blue: "#1a8cd8",
+                    dark: "#14171A",
+                    textblack: "#0f1419",
+                    gray: "#536471",
+                    lightgray: "#AAB8C2",
+                    lightergray: "#E1E8ED",
+                    lightestgray: "#F5F8FA",
+                    white: "#fff",
+                    love: "#f91880",
+                    retweet: "#00ba7c",
+                },
                 dim: {
                     50: "#5F99F7",
                     100: "#5F99F7",
@@ -19,6 +33,13 @@ module.exports = {
             },
             animation: {
                 "spin-fast": "spin 0.5s linear infinite",
+            },
+            fontSize: {
+                twitter: {
+                    heading: "20px",
+                    normal: "15px",
+                    small: "13px",
+                },
             },
         },
     },
