1872b84c819b8a8ca2aaaaff280fdb4fb575ec55
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-10 00:39:38 +0200

    setting main tailwindcss custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                twitter: {
+                    blue: "#1d9bf0",
+                    blue: "#1a8cd8",
+                    dark: "#14171A",
+                    textblack: "#0f1419",
+                    gray: "#536471",
+                    lightgray: "#AAB8C2",
+                    lightergray: "#E1E8ED",
+                    lightestgray: "#F5F8FA",
+                    white: "#fff",
+                    love: "#f91880",
+                    retweet: "#00ba7c",
+                },
+            },
+            fontSize: {
+                twitter: {
+                    heading: "20px",
+                    normal: "15px",
+                    small: "13px",
+                },
+            },
+            // fontFamily: {
+            //     twitter: ["TwitterChirp", "-apple-system", "system-ui", "Segoe UI", "Roboto", "Helvetica", "Arial", "sans-serif"],
+            // },
+        },
+        plugins: [],
     },
-    plugins: [],
 };
