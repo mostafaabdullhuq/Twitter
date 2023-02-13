37468edc89dd9353ba8760422f9dedc6f9547767
hagarahmed68896 <76259845+hagarahmed68896@users.noreply.github.com>
2023-02-14 00:44:58 +0200

    add edit-profile popup

--- front/docs/signup.js
+++ front/docs/signup.js
@@ -1,7 +1,8 @@
 const toggleButton = document.getElementById("toggle-popup");
 const popup = document.getElementById("popup");
 const closeButton = document.getElementById("close-popup");
-
+const popupEdit = document.getElementById("popupEdit");
+const toggleButton1 = document.getElementById("toggle-popup1");
 
 toggleButton.addEventListener("click", () => {
   popup.style.display = "block";
@@ -9,6 +10,16 @@ toggleButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   popup.style.display = "none";
+});
+
+
+
+toggleButton1.addEventListener("click", () => {
+  popupEdit.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+  popupEdit.style.display = "none";
 });
 
 
