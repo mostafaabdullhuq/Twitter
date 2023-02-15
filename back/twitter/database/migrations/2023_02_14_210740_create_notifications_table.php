0025cb912fb1063cf730e4bcaa792800c9caec5a
Mohamed Abdelkarim <mohamed_abdelkarim@protonmail.com>
2023-02-15 19:59:50 +0200

    Update 2023_02_14_210740_create_notifications_table.php

--- back/twitter/database/migrations/2023_02_14_210740_create_notifications_table.php
+++ back/twitter/database/migrations/2023_02_14_210740_create_notifications_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->morphs('parent');
             // $table->string('type');
-            $table->timestamps('created_at');
+            $table->timestamps('');
             $table->boolean('is_read');
         });
     }
