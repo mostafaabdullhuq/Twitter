f5a83dd82e426351ccb5e1e93ef211d5807336a0
hagarahmed68896 <76259845+hagarahmed68896@users.noreply.github.com>
2023-02-15 23:26:29 +0200

    add signin&up components

--- Twitter/src/app/app-routing.module.ts
+++ Twitter/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './Components/signin/signin.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',
+  component:SigninComponent},
+  {path:'signup',
+  component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
