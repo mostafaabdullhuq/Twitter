765147730f25264bca617ba0799cca255017f2f3
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-17 02:37:53 +0200

    fixed problem with routes 404

--- back/twitter/routes/api.php
+++ back/twitter/routes/api.php
@@ -1,14 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+
+
 Route::group([
 
     'middleware' => 'api',
     'prefix' => 'auth'
 
 ], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
+
+
+
+
+
+
+# php artisan key:generate
+# php artisan jwt:secret
+# jwt-auth secret [v9m6605MEHxcHrgC41BFyJbAOY19UH8Ypa1udnthDFuLN96IUVgD3iDgI2zfLKTa] set successfully.
