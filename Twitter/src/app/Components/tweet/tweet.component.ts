1984c0a905bb1d1ead3b83630cd8f64d7668a7b3
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-17 15:41:20 +0200

    added tweet service

--- Twitter/src/app/Components/tweet/tweet.component.ts
+++ Twitter/src/app/Components/tweet/tweet.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tweet',
   templateUrl: './tweet.component.html',
-  styleUrls: ['./tweet.component.css']
+  styleUrls: ['./tweet.component.css'],
 })
 export class TweetComponent {
-
+  @Input() tweets: any;
 }
