dab320f51a2aa7bcde30d5b6859b64705e2fb7e1
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-19 02:01:22 +0200

    fixed packages incompatible problem

--- Twitter/src/app/Services/auth.service.ts
+++ Twitter/src/app/Services/auth.service.ts
@@ -3,19 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient,) { }
- private baseUrl = 'http://127.0.0.1:8000/api/auth';
-  signup(data: any){
-    return this.http.post(`${this.baseUrl}/signup`, data)
+  constructor(private http: HttpClient) {}
+  private baseUrl = 'http://127.0.0.1:8000/api/auth';
+  signup(data: any) {
+    return this.http.post(`${this.baseUrl}/signup`, data, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
   }
-  login(data: any){
-    return this.http.post(`${this.baseUrl}/login`, data)
-
+  login(data: any) {
+    return this.http.post(`${this.baseUrl}/login`, data, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
   }
 
   public loggedIn$ = new BehaviorSubject<boolean>(false);
-
 }
