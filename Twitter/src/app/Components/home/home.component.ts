b5b78a840afe0f3127c257d2a161974ed47a25f0
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-23 09:09:54 +0200

    tweet create

--- Twitter/src/app/Components/home/home.component.ts
+++ Twitter/src/app/Components/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TokenService } from 'src/app/Services/token.service';
@@ -14,8 +20,11 @@ export class HomeComponent implements OnInit {
   public user: any;
   public tweetForm = new FormGroup({
     text: new FormControl(null, [Validators.max(1000)]),
-    // media: new FormControl(null, [Validators.required]),
+    media: new FormControl(null, []),
   });
+  public tweetMedia: any;
+
+  @ViewChild('tweetBox') tweetBox!: ElementRef;
 
   constructor(
     public myRoute: ActivatedRoute,
@@ -45,5 +54,53 @@ export class HomeComponent implements OnInit {
         },
       });
     }
+  }
+
+  // ngAfterViewInit(): void {
+  //   let tweetBox = this.tweetBox.nativeElement;
+  //   tweetBox.addEventListener('input', () => {
+  //     console.log('input');
+  //   });
+  // }
+
+  tweetSubmit() {
+    const media = this.tweetForm.value.media;
+    const text = this.tweetForm.value.text;
+    if (media || text) {
+      console.log(this.tweetForm);
+    } else {
+      this.tweetForm.setErrors({ invalid: true });
+    }
+
+    // if (this.tweetForm.valid) {
+    //   this.httpClient
+    //     .postTweet(this.tweetForm.value.text, this.tweetForm.value.media)
+    //     .subscribe({
+    //       next: (data: any) => {
+    //         this.tweets.unshift(data);
+    //         this.tweetForm.reset();
+    //       },
+    //       error: (err) => {
+    //         console.log(err);
+    //       },
+    //     });
+    // }
+  }
+
+  handleEditor(event: any) {
+    console.log(event);
+  }
+
+  inputChange(data: any) {
+    let file = data.target.files ? data.target.files[0] : null;
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+      this.tweetMedia = e.target?.result;
+    };
+  }
+
+  removeMedia() {
+    this.tweetMedia = null;
   }
 }
