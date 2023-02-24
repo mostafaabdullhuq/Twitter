d07130dbdab96f5a73ed5bc43545a07d2c0c474e
hagarahmed68896 <76259845+hagarahmed68896@users.noreply.github.com>
2023-02-24 23:46:50 +0200

    get data into edit profile

--- Twitter/src/app/Components/edit-profile/edit-profile.component.ts
+++ Twitter/src/app/Components/edit-profile/edit-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 import { TweetsService } from 'src/app/Services/tweets.service';
 
 @Component({
@@ -7,16 +9,22 @@ import { TweetsService } from 'src/app/Services/tweets.service';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
-  constructor( public tweetsClient: TweetsService) {}
+
+  constructor( public tweetsClient: TweetsService,
+    public myActivate:ActivatedRoute, public myservice:AuthService) {}
   public user: any;
   ngOnInit(): void {
     this.tweetsClient.getAuthedTweets().subscribe({
       next: (data: any) => {
         this.user = data.user;
+        console.log(this.user);
       },
       error: (err) => {
         console.log(err);
       },
     });
   }
+  // updateUser(){
+  // this.myservice.updateUser(this.id,this.user).subscribe({});
+  // }
 }
