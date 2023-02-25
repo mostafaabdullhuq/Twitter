919fbc17dc8c696c7e2e3f140bcc1eb5bf2768b0
mohamedmek <mohamed_abdelkarim@protonmail.com>
2023-02-25 03:54:38 +0200

    change delete route

--- back/twitter/routes/api.php
+++ back/twitter/routes/api.php
@@ -35,7 +35,6 @@ Route::group([
     Route::get('index', [UserController::class, 'index']);
     Route::post('update', [UserController::class, 'update']);
     Route::delete('delete', [UserController::class, 'destroy']);
-
     Route::post('follow-unfollow', [FollowingController::class, 'store']);
 });
 
