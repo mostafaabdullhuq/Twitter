7b29b5b89d2ce8842491473815454e510aaf2d73
hagarahmed68896 <76259845+hagarahmed68896@users.noreply.github.com>
2023-02-26 13:35:50 +0200

    handel null to update data

--- back/twitter/app/Http/Controllers/UpdateDataController.php
+++ back/twitter/app/Http/Controllers/UpdateDataController.php
@@ -28,7 +28,7 @@ class UpdateDataController extends Controller
     private function updateData($request)
     {
         $user = User::whereEmail($request->email)->first();
-
+    
         // Save the old values in variables
         $oldEmail = $user->email;
         $oldFirstName = $user->first_name;
@@ -39,46 +39,61 @@ class UpdateDataController extends Controller
         $oldWebsite = $user->website;
         $oldPhoneNumber = $user->phone_number;
         $oldDateOfBirth = $user->date_of_birth;
-
-        // Update the user data if the request data is not empty
-        if (!empty($request->email)) {
+    
+        // Update the user data if the request data is not null
+        if ($request->email !== null) {
             $user->email = $request->email;
         }
-        if (!empty($request->first_name)) {
+        if ($request->first_name !== null) {
             $user->first_name = $request->first_name;
         }
-        if (!empty($request->last_name)) {
+        if ($request->last_name !== null) {
             $user->last_name = $request->last_name;
         }
-        if (!empty($request->username)) {
+        if (!$request->username || trim($request->username) == '') {
+            $user->username = null;
+        } else {
             $user->username = $request->username;
         }
-        if (!empty($request->bio)) {
+        if (!$request->bio || trim($request->bio) == '') {
+            $user->bio = null;
+        } else {
             $user->bio = $request->bio;
         }
-        if (!empty($request->location)) {
+        if (!$request->location || trim($request->location) == '') {
+            $user->location = null;
+        } else {
             $user->location = $request->location;
         }
-        if (!empty($request->website)) {
+    
+        if (!$request->website || trim($request->website) == '') {
+            $user->website = null;
+        } else {
             $user->website = $request->website;
         }
-        if (!empty($request->phone_number)) {
+        if (!$request->phone_number || trim($request->phone_number) == '') {
+            $user->phone_number = null;
+        } else {
             $user->phone_number = $request->phone_number;
         }
-        if (!empty($request->date_of_birth)) {
+        if (!$request->date_of_birth || trim($request->date_of_birth) == '') {
+            $user->date_of_birth = null;
+        } else {
             $user->date_of_birth = $request->date_of_birth;
         }
-
+    
         // Save the changes to the database
         $user->save();
-
+    
         // Check if any data was updated and return a success message
-        if ($user->email != $oldEmail || $user->first_name != $oldFirstName || $user->last_name != $oldLastName || $user->username != $oldUserName || $user->bio != $oldBio ||
-            $user->location != $oldLocation || $user->website != $oldWebsite || $user->phone_number != $oldPhoneNumber ||
-            $user->date_of_birth != $oldDateOfBirth) {
+        if ($user->email !== $oldEmail || $user->first_name !== $oldFirstName || $user->last_name !== $oldLastName || $user->username !== $oldUserName || $user->bio !== $oldBio ||
+            $user->location !== $oldLocation || $user->website !== $oldWebsite || $user->phone_number !== $oldPhoneNumber ||
+            $user->date_of_birth !== $oldDateOfBirth) {
             return response()->json(['data' => 'Data Successfully Changed'], Response::HTTP_CREATED);
         } else {
             return response()->json(['data' => 'No Data Changed'], Response::HTTP_OK);
         }
     }
+    
+    
 }
