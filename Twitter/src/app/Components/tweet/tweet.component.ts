b61f43b6a8ffdbb413aa2210d013468d0d45e145
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-26 08:03:17 +0200

    upload tweet media

--- Twitter/src/app/Components/tweet/tweet.component.ts
+++ Twitter/src/app/Components/tweet/tweet.component.ts
@@ -2,28 +2,32 @@
 // that can be clicked on. It uses the sanitizer to bypass security restrictions and allows
 // the html to be rendered in the tweet component.
 
-import { Component, Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
-import { RouterModule , RouterLink } from '@angular/router';
+import { RouterModule, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-tweet',
   templateUrl: './tweet.component.html',
   styleUrls: ['./tweet.component.css'],
 })
 export class TweetComponent {
-  constructor(private sanitizer: DomSanitizer ) {}
+  constructor(private sanitizer: DomSanitizer) {}
   formatTweetText(text: string): SafeHtml {
-    const hashtagRegex = /#[a-zA-Z0-9_]+/g;
-    const mentionRegex = /@[a-zA-Z0-9_]+/g;
-    const hashtagTemplate = '<a href="#" class="hashtag">$&</a>';
-    const mentionTemplate = '<a href="#" class="hashtag">$&</a>';
+    if (text) {
+      const hashtagRegex = /#[a-zA-Z0-9_]+/g;
+      const mentionRegex = /@[a-zA-Z0-9_]+/g;
+      const hashtagTemplate = '<a href="#" class="hashtag">$&</a>';
+      const mentionTemplate = '<a href="#" class="hashtag">$&</a>';
 
-    const formattedText = text
-      .replace(hashtagRegex, hashtagTemplate)
-      .replace(mentionRegex, mentionTemplate);
+      const formattedText = text
+        .replace(hashtagRegex, hashtagTemplate)
+        .replace(mentionRegex, mentionTemplate);
 
-    return this.sanitizer.bypassSecurityTrustHtml(formattedText);
+      return this.sanitizer.bypassSecurityTrustHtml(formattedText);
+    } else {
+      return '';
+    }
   }
 
   // likeTweet(tweetId: number) {
