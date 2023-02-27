0da916bbda90d81f605121183385de78200d116e
mohamedmek <mohamed_abdelkarim@protonmail.com>
2023-02-27 05:07:38 +0200

    git users

--- Twitter/src/app/Components/follow-recommendations/follow-recommendations.component.ts
+++ Twitter/src/app/Components/follow-recommendations/follow-recommendations.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { UsersService } from 'src/app/Services/users.service';
 
 @Component({
   selector: 'app-follow-recommendations',
   templateUrl: './follow-recommendations.component.html',
   styleUrls: ['./follow-recommendations.component.css']
 })
-export class FollowRecommendationsComponent {
+export class FollowRecommendationsComponent implements OnInit{
+  constructor(public myService:UsersService){}
 
+  users:any;
+  ngOnInit(): void {
+    
+    this.myService.getAllUsers().subscribe(
+      {
+        next:(data)=>{
+            this.users = data;
+        },
+        error:(err)=>{err},
+      })
+  }
 }
+
---
+++ Twitter/src/app/Services/users.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Token } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { TokenService } from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private httpClient:HttpClient, public token: TokenService) { }
+
+  private BASE_URL = 'http://127.0.0.1:8000/api/user';
+
+  getAllUsers(){
+    const accessToken = this.token.get();
+
+    return this.httpClient.get(this.BASE_URL +'/get-all',
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+  }
+}
