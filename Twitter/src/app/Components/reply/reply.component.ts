fefa8df4e348b1c685f250d65b72fbb2b683d7e6
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-28 07:45:01 +0200

    like button interactivity when click, tweet text and repply text auto formaatiing

--- Twitter/src/app/Components/reply/reply.component.ts
+++ Twitter/src/app/Components/reply/reply.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 // import { TweetsService } from 'src/app/Services/tweets.service';
-import { RouterModule , RouterLink, ActivatedRoute } from '@angular/router';
+import { RouterModule, RouterLink, ActivatedRoute } from '@angular/router';
 import { ReplyService } from 'src/app/Services/reply.service';
 
 @Component({
@@ -11,20 +12,38 @@ import { ReplyService } from 'src/app/Services/reply.service';
 export class ReplyComponent {
   constructor(
     public myRoute: ActivatedRoute,
-     public httpClient:ReplyService,) {}
+    public httpClient: ReplyService,
+    private sanitizer: DomSanitizer
+  ) {}
 
-     //like
+  //like
   // isLiked: boolean = false;
-     likesCount(replyID:any){
-      this.httpClient.getLikesCount( replyID ).subscribe({
-        next: (data: any) => {
-          console.log(data);
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
-    }
-    
+  likesCount(replyID: any) {
+    this.httpClient.getLikesCount(replyID).subscribe({
+      next: (data: any) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
   @Input() replies: any;
+  formatTweetText(text: string): SafeHtml {
+    if (text) {
+      const hashtagRegex = /#([\p{Pc}\p{N}\p{L}\p{Mn}]+)/gu;
+      const mentionRegex = /@([\p{Pc}\p{N}\p{L}\p{Mn}]+)/gu;
+      const hashtagTemplate = '<a href="#" class="hashtag">$&</a>';
+      const mentionTemplate = '<a href="#" class="hashtag">$&</a>';
+
+      const formattedText = text
+        .replace(hashtagRegex, hashtagTemplate)
+        .replace(mentionRegex, mentionTemplate);
+
+      return this.sanitizer.bypassSecurityTrustHtml(formattedText);
+    } else {
+      return '';
+    }
+  }
 }
