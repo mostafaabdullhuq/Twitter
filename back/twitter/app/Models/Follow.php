e3c7d88a6127f848ea2e93cf0fde2d7396f66f54
mohamedmek <mohamed_abdelkarim@protonmail.com>
2023-02-28 15:09:42 +0200

    follow model relation

--- back/twitter/app/Models/Follow.php
+++ back/twitter/app/Models/Follow.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'following_id',
+        'follower_id',
+    ];
+
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+    }
 }
 
