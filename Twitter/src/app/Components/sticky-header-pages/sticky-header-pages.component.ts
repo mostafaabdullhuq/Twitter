9e920b0ae0542f1cf6c12736c807a6b82130055c
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-03-01 04:42:06 +0200

    sticky header pages with custom title

--- Twitter/src/app/Components/sticky-header-pages/sticky-header-pages.component.ts
+++ Twitter/src/app/Components/sticky-header-pages/sticky-header-pages.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sticky-header-pages',
   templateUrl: './sticky-header-pages.component.html',
-  styleUrls: ['./sticky-header-pages.component.css']
+  styleUrls: ['./sticky-header-pages.component.css'],
 })
 export class StickyHeaderPagesComponent {
-
+  @Input() title: string = 'Tweet';
+  @Input() backURL: string = '/home';
 }
