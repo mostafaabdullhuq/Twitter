3413d5f4ea7d7721e13b2db1ef6fa18244eef2f5
SaRa-Fathi <sarafathi607@gmail.com>
2023-03-02 06:11:37 +0200

    in msgs

---
+++ back/twitter/app/Events/MessageEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public string $username, public string $message)
+    {
+    }
+    //for the channel name
+    public function broadcastOn()
+    {
+        // return ['Twitty'];
+        return new Channel('TwittyChat');
+    }
+    //for the message name
+    public function broadcastAs()
+    {
+        return 'message chatting';
+    }
+}
--- back/twitter/config/broadcasting.php
+++ back/twitter/config/broadcasting.php
@@ -35,12 +35,9 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+            'options' =>  [
+                'cluster' => 'mt1',
+                'useTLS' => true
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
@@ -68,3 +65,10 @@ return [
     ],
 
 ];
+// 'options' =>  [
+// //     'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+// //     'port' => env('PUSHER_PORT', 443),
+// //     'scheme' => env('PUSHER_SCHEME', 'https'),
+//     // 'encrypted' => true,
+//     // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+// ],
