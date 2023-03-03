b77e83a878b8cc664afe03c8ce035a9f93b70cda
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-03-03 06:03:06 +0200

    done retweet and post tweet

--- Twitter/src/app/Components/tweet-popup/tweet-popup.component.ts
+++ Twitter/src/app/Components/tweet-popup/tweet-popup.component.ts
@@ -7,29 +7,19 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { TokenService } from 'src/app/Services/token.service';
 import { TweetsService } from 'src/app/Services/tweets.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { UserService } from 'src/app/Services/user.service';
-import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-tweet-popup',
   templateUrl: './tweet-popup.component.html',
   styleUrls: ['./tweet-popup.component.css'],
 })
-export class TweetPopupComponent implements OnInit {
+export class TweetPopupComponent {
   constructor(
     private httpClient: TweetsService,
-    private userService: UserService,
-    private activatedRouter: ActivatedRoute,
-    private router: Router,
-    private tokenService: TokenService,
-    private sanitizer: DomSanitizer,
-    private authService: AuthService
+    private sanitizer: DomSanitizer
   ) {}
 
   @Output() isClosed: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -54,29 +44,11 @@ export class TweetPopupComponent implements OnInit {
     this.isClosed.emit(true);
   }
 
-  replySubmit() {
-    // let reply = {
-    //   text: this.retweetForm.value.text,
-    // };
-    // this.httpClient.createReply(reply, this.tweetID).subscribe({
-    //   next: (data: any) => {
-    //     this.tweet.replies.unshift(data);
-    //     this.tweet.replies_count += 1;
-    //     this.retweetForm.reset();
-    //   },
-    //   error: (err) => {
-    //     this.error = err;
-    //   },
-    // });
-  }
-
   shareTweet() {
     let retweetText = this.retweetForm.value.text ?? null;
     this.httpClient.postRetweet(this.tweet?.id, retweetText).subscribe({
       next: (data) => {
         this.tweet = data;
-        console.log(this.tweet);
-        console.log('Tweet shared successfully');
       },
       error: (err) => {
         this.error = err;
@@ -84,49 +56,86 @@ export class TweetPopupComponent implements OnInit {
     });
     this.closePopup();
   }
+  inputChange(data: any) {
+    this.tweetMedia = [];
+    this.tweetMediaFiles = Array.from(
+      data.target.files ? data.target.files : null
+    );
+    Object.keys(this.tweetMediaFiles).forEach((key: any) => {
+      this.tweetMediaTypes.push(
+        this.tweetMediaFiles[key].type.split('/')[0] == 'image' ? 1 : 2
+      );
+      let reader = new FileReader();
+      reader.readAsDataURL(this.tweetMediaFiles[key]);
+      reader.onload = (e) => {
+        this.tweetMedia.push(e.target?.result);
+        this.currentMediaSrc = this.tweetMedia[0];
+        this.currentMediaType = this.tweetMediaTypes[0];
+      };
+    });
+  }
 
-  // handleMedia(type: any, container: any, tweet: any) {
-  // let nextIndex;
-  // let currentSrc = container.children[0].children[0].src;
-  // Object.entries(tweet.media).forEach((value: any, index: any) => {
-  //   let mediaObj = value[1];
-  //   if (currentSrc === mediaObj.media_url) {
-  //     if (type === 1) {
-  //       nextIndex = index + 1 >= tweet.media.length ? 0 : index + 1;
-  //     } else {
-  //       nextIndex = index - 1 < 0 ? tweet.media.length - 1 : index - 1;
-  //     }
-  //     console.log(nextIndex);
-  //     container.children[0].children[0].src =
-  //       tweet.media[nextIndex].media_url;
-  //     container.children[0].children[0].src =
-  //       tweet.media[nextIndex].media_url;
-  //   }
-  // });
-  // }
+  handleMedia(type: any) {
+    let elementIndex = 0;
+    this.tweetMedia.forEach((element: any, index: any) => {
+      if (this.currentMediaSrc == element) {
+        if (type == 1) {
+          elementIndex = index + 1 >= this.tweetMedia.length ? 0 : index + 1;
+        } else {
+          elementIndex = index - 1 < 0 ? this.tweetMedia.length - 1 : index - 1;
+        }
+      }
+    });
+    this.currentMediaSrc = this.tweetMedia[elementIndex];
+    this.currentMediaType = this.tweetMediaTypes[elementIndex];
+    this.currentMediaIndex = elementIndex;
+  }
 
-  ngOnInit(): void {
-    console.log(this.tweet);
-    console.log(this.user);
-    console.log(this.popupType);
+  removeMedia() {
+    this.tweetMedia.splice(this.currentMediaIndex, 1);
+    this.tweetMediaTypes.splice(this.currentMediaIndex, 1);
+    Object.entries(this.tweetMediaFiles).forEach(([key, value]) => {
+      if (key == this.currentMediaIndex) {
+        delete this.tweetMediaFiles[key];
+      }
+    });
+    this.tweetMediaFiles = Array.from(this.tweetMediaFiles).filter(
+      (item: any) => item != undefined
+    );
+    this.currentMediaSrc = this.tweetMedia[0];
+    this.currentMediaType = this.tweetMediaTypes[0];
+    this.currentMediaIndex = 0;
+    if (!this.tweetMedia.length) {
+      this.tweetForm.patchValue({ media: null });
+    }
+  }
 
-    // const tweetID = this.activatedRouter.snapshot.params['id'];
-    // this.httpClient.getTweetById(+tweetID).subscribe({
-    //   next: (data) => {
-    //     this.tweet = data;
-    //   },
-    //   error: (err) => {
-    //     this.error = err;
-    //   },
-    // });
-    // this.httpClient.addView(+tweetID).subscribe({
-    //   next: (data) => {
-    //     this.tweet = data;
-    //   },
-    //   error: (err) => {
-    //     this.error = err;
-    //   },
-    // });
+  tweetSubmit() {
+    const media: any = this.tweetForm.value.media ?? null;
+    const type: number = 1;
+    const text: any = this.tweetForm.value.text ?? null;
+    let tweet: any = {};
+    if (this.tweetMediaFiles || text) {
+      let postData = new FormData();
+      Object.keys(this.tweetMediaFiles).forEach((key: any) => {
+        postData.append('files[]', this.tweetMediaFiles[key]);
+      });
+      text ? postData.append('text', text) : false;
+      this.httpClient.createTweet(postData).subscribe({
+        next: (data: any) => {
+          // this.tweets.unshift(data);
+          this.tweetForm.reset();
+          this.tweetMedia = [];
+          this.tweetMediaFiles = [];
+        },
+        error: (err: any) => {
+          console.log(err);
+        },
+      });
+      this.closePopup();
+    } else {
+      this.tweetForm.setErrors({ invalid: true });
+    }
   }
 
   formatTweetText(text: any): SafeHtml {
@@ -169,170 +178,4 @@ export class TweetPopupComponent implements OnInit {
     text: new FormControl(null, [Validators.max(1000)]),
     media: new FormControl(null, []),
   });
-
-  // infinite scrolling logic
-  // public observer: any = new IntersectionObserver((entries: any) => {
-  //   entries.forEach((entry: any) => {
-  //     if (entry.isIntersecting) {
-  //       if (this.myRoute.snapshot?.url[1]?.path === 'following') {
-  //         this.tweetClient.getFollowingTweets(this.nextCursor).subscribe({
-  //           next: (data: any) => {
-  //             this.tweets = this.tweets.concat(data.tweets);
-  //             this.nextCursor = data.nextCursor;
-  //             if (data.nextCursor == null) {
-  //               this.observer.disconnect();
-  //               this.isLoadingDone = true;
-  //             }
-  //           },
-  //           error: (err) => {
-  //             console.log(err);
-  //           },
-  //         });
-  //       } else {
-  //         this.tweetClient.getForYouTweets(this.nextCursor2).subscribe({
-  //           next: (data: any) => {
-  //             this.tweets = this.tweets.concat(data.tweets);
-  //             this.nextCursor2 = data.nextCursor;
-  //             if (data.nextCursor2 == null) {
-  //               this.isLoadingDone = true;
-
-  //               this.observer.disconnect();
-  //             }
-  //           },
-  //           error: (err) => {
-  //             console.log(err);
-  //           },
-  //         });
-  //       }
-  //     }
-  //   });
-  // });
-
-  ngAfterViewChecked(): void {
-    // let lastTweet = document.querySelector('.tweet:last-child');
-    // if (this.myRoute.snapshot?.url[1]?.path === 'following') {
-    //   if (lastTweet && this.nextCursor != null) {
-    //     this.isLoadingDone = false;
-    //     this.observer.observe(lastTweet);
-    //   }
-    // } else {
-    //   if (lastTweet && this.nextCursor2 != null) {
-    //     this.isLoadingDone = false;
-    //     this.observer.observe(lastTweet);
-    //   }
-    // }
-  }
-
-  // ngOnInit(): void {
-  //   if (this.myRoute.snapshot?.url[1]?.path === 'following') {
-  //     this.tweetClient.getFollowingTweets(this.nextCursor).subscribe({
-  //       next: (data: any) => {
-  //         this.tweets = data.tweets;
-  //         this.nextCursor = data.nextCursor;
-  //         this.authService.getUser().subscribe({
-  //           next: (data: any) => {
-  //             this.user = data;
-  //           },
-  //           error: (err: any) => {
-  //             this.user = this.Token.getUser();
-  //           },
-  //         });
-  //       },
-  //       error: (err) => {
-  //         console.log(err);
-  //       },
-  //     });
-  //   } else {
-  //     this.tweetClient.getForYouTweets(this.nextCursor2).subscribe({
-  //       next: (data: any) => {
-  //         this.tweets = data.tweets;
-  //         this.nextCursor2 = data.nextCursor;
-  //       },
-  //       error: (err) => {
-  //         console.log(err);
-  //       },
-  //     });
-  //   }
-  // }
-
-  tweetSubmit() {
-    // const media: any = this.tweetForm.value.media ?? null;
-    // const type: number = 1;
-    // const text: any = this.tweetForm.value.text ?? null;
-    // let tweet: any = {};
-    // if (this.tweetMediaFiles || text) {
-    //   let postData = new FormData();
-    //   Object.keys(this.tweetMediaFiles).forEach((key: any) => {
-    //     postData.append('files[]', this.tweetMediaFiles[key]);
-    //   });
-    //   text ? postData.append('text', text) : false;
-    //   this.tweetClient.createTweet(postData).subscribe({
-    //     next: (data: any) => {
-    //       this.tweets.unshift(data);
-    //       this.tweetForm.reset();
-    //       this.tweetMedia = [];
-    //       this.tweetMediaFiles = [];
-    //     },
-    //     error: (err) => {
-    //       console.log(err);
-    //     },
-    //   });
-    // } else {
-    //   this.tweetForm.setErrors({ invalid: true });
-    // }
-  }
-
-  inputChange(data: any) {
-    // this.tweetMedia = [];
-    // this.tweetMediaFiles = Array.from(
-    //   data.target.files ? data.target.files : null
-    // );
-    // Object.keys(this.tweetMediaFiles).forEach((key: any) => {
-    //   this.tweetMediaTypes.push(
-    //     this.tweetMediaFiles[key].type.split('/')[0] == 'image' ? 1 : 2
-    //   );
-    //   let reader = new FileReader();
-    //   reader.readAsDataURL(this.tweetMediaFiles[key]);
-    //   reader.onload = (e) => {
-    //     this.tweetMedia.push(e.target?.result);
-    //     this.currentMediaSrc = this.tweetMedia[0];
-    //     this.currentMediaType = this.tweetMediaTypes[0];
-    //   };
-    // });
-  }
-
-  handleMedia(type: any) {
-    //   let elementIndex = 0;
-    //   this.tweetMedia.forEach((element: any, index: any) => {
-    //     if (this.currentMediaSrc == element) {
-    //       if (type == 1) {
-    //         elementIndex = index + 1 >= this.tweetMedia.length ? 0 : index + 1;
-    //       } else {
-    //         elementIndex = index - 1 < 0 ? this.tweetMedia.length - 1 : index - 1;
-    //       }
-    //     }
-    //   });
-    //   this.currentMediaSrc = this.tweetMedia[elementIndex];
-    //   this.currentMediaType = this.tweetMediaTypes[elementIndex];
-    //   this.currentMediaIndex = elementIndex;
-  }
-
-  removeMedia() {
-    // this.tweetMedia.splice(this.currentMediaIndex, 1);
-    // this.tweetMediaTypes.splice(this.currentMediaIndex, 1);
-    // Object.entries(this.tweetMediaFiles).forEach(([key, value]) => {
-    //   if (key == this.currentMediaIndex) {
-    //     delete this.tweetMediaFiles[key];
-    //   }
-    // });
-    // this.tweetMediaFiles = Array.from(this.tweetMediaFiles).filter(
-    //   (item: any) => item != undefined
-    // );
-    // this.currentMediaSrc = this.tweetMedia[0];
-    // this.currentMediaType = this.tweetMediaTypes[0];
-    // this.currentMediaIndex = 0;
-    // if (!this.tweetMedia.length) {
-    //   this.tweetForm.patchValue({ media: null });
-    // }
-  }
 }
